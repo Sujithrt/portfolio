bc2f3b5c61bad226e44bf372d3d556d865ac8664
stellak1 <stellak1@asu.edu>
2024-09-12 22:07:55 -0700

    added full name

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typography from '@mui/material/Typography';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
@@ -16,11 +17,13 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/Sujithrt" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/sujith-tellakula/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-            <a href="mailto:sujithramprasad@gmail.com" rel="noreferrer"><EmailIcon/></a>
+            <a href="https://github.com/Sujithrt" target="_blank" rel="noreferrer"><GitHubIcon /></a>
+            <a href="https://www.linkedin.com/in/sujith-tellakula/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+            <a href="mailto:sujithramprasad@gmail.com" rel="noreferrer"><EmailIcon /></a>
           </div>
-          <h1>Sujith Tellakula</h1>
+          <Typography variant="h2" gutterBottom>
+            <strong>Sujith Ramprasad Tellakula</strong>
+          </Typography>
           <p>Passionate software developer with expertise in full-stack development, cloud computing, and AI integration. Seeking to apply my skills in building scalable and efficient solutions to drive innovation and enhance user experience.</p>
           <a href="https://myawsbucket-sujith.s3.us-west-2.amazonaws.com/Resume_SDE.pdf" download="Sujith_Tellakula_Resume.pdf" target="_blank" rel="noreferrer">
             <Button variant="contained" startIcon={<DownloadIcon />}>
@@ -28,9 +31,9 @@ function Main() {
             </Button>
           </a>
           <div className="mobile_social_icons">
-            <a href="https://github.com/Sujithrt" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/sujith-tellakula/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-            <a href="mailto:sujithramprasad@gmail.com" rel="noreferrer"><EmailIcon/></a>
+            <a href="https://github.com/Sujithrt" target="_blank" rel="noreferrer"><GitHubIcon /></a>
+            <a href="https://www.linkedin.com/in/sujith-tellakula/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+            <a href="mailto:sujithramprasad@gmail.com" rel="noreferrer"><EmailIcon /></a>
           </div>
         </div>
       </div>

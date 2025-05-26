16ab5047c0271a62cb50be57341c2a83e5e2d53c
Sujithrt <sujithramprasad@gmail.com>
2025-05-25 18:29:09 -0700

    added education to Timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,6 +1,6 @@
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../assets/styles/Timeline.scss'
@@ -13,14 +13,14 @@ function Timeline() {
         <VerticalTimeline>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="January 2024 - Present"
+            date="January 2024 - May 2025"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">AI Full Stack Developer (On-campus)</h3>
-            <h4 className="vertical-timeline-element-subtitle">Technology at ASU, Tempe, USA</h4>
+            <h3 className="vertical-timeline-element-title">AI Full Stack Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Enterprise Technology at ASU, Tempe, USA</h4>
             <p>
-              AI Application Development, GenAI Integration, Data Pipeline Design, Performance Optimization
+              AI Application Development, GenAI Integration, Data Pipeline Design, Performance Optimization.
             </p>
           </VerticalTimelineElement>
 
@@ -35,7 +35,20 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Software Development Engineer Intern</h3>
             <h4 className="vertical-timeline-element-subtitle">Amazon Web Services, Seattle, USA</h4>
             <p>
-              API Development, AWS Integration, UI Development, Test Automation
+              API Development, AWS Integration, UI Development, Test Automation.
+            </p>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="August 2023 - May 2025"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
+          >
+            <h3 className="vertical-timeline-element-title">M.S. in Computer Science</h3>
+            <h4 className="vertical-timeline-element-subtitle">Arizona State University, Tempe, USA</h4>
+            <p>
+              Cloud Computing, AI/ML, Foundations of Algorithms, Semantic Web Mining, Data Processing at Scale, Software Testing.
             </p>
           </VerticalTimelineElement>
 
@@ -48,7 +61,7 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Software Engineer</h3>
             <h4 className="vertical-timeline-element-subtitle">Oracle Cerner, Bengaluru, India</h4>
             <p>
-              Full-stack Development, Ruby on Rails, UI/UX Design, System Integration, Security Enhancements
+              Full-stack Development, Ruby on Rails, UI/UX Design, System Integration, Security Enhancements.
             </p>
           </VerticalTimelineElement>
 
@@ -61,7 +74,20 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Machine Learning Intern</h3>
             <h4 className="vertical-timeline-element-subtitle">Ideapoke Technologies Pvt Ltd, Bengaluru, India</h4>
             <p>
-              Data Collection, Data Cleaning, Model Training, Python, scikit-learn
+              Data Collection, Data Cleaning, Model Training, Python, scikit-learn.
+            </p>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="August 2023 - May 2025"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
+          >
+            <h3 className="vertical-timeline-element-title">B.E. in Information Science and Engineering</h3>
+            <h4 className="vertical-timeline-element-subtitle">Visveswaraya Technological University, Bengaluru, India</h4>
+            <p>
+              Operating Systems, Data Structures, DBMS, Computer Networks, Programming Languages.
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
